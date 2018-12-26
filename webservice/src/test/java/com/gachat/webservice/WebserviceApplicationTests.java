86ced836e780a267a786ae2a50f0c3610d67cae1
wym <793717561@qq.com>
2018-12-26 22:49:51 +0800

    初始化项目
    初始化项目

---
+++ webservice/src/test/java/com/gachat/webservice/WebserviceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gachat.webservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
