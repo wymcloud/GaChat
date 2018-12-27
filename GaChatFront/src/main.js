a8659a5564151575580433541140ac4c4356997e
wym <793717561@qq.com>
2018-12-27 22:58:07 +0800

    提交代码

--- GaChatFront/src/main.js
+++ GaChatFront/src/main.js
@@ -6,6 +6,16 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// 引入样式重置
+import '../static/css/reset.css'
+
+// 引入 ElementUI 响应式断点
+import 'element-ui/lib/theme-chalk/display.css';
+
+// 引入及使用 ElementUI
+import {Row, Col} from 'element-ui';
+Vue.use(Row).use(Col);
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
